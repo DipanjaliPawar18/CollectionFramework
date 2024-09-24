0ee5fa8b8ff855e1d6bdb35aba2c5d327acade37
Dipanjali <122343977+DipanjaliPawar18@users.noreply.github.com>
2024-09-24 20:22:10 +0530

    Add files via upload
    LinkedList of String

---
+++ StrinArrayList.java
@@ -0,0 +1,25 @@
+package com.listInterfaceDemo;
+
+import java.util.ArrayList;
+
+public class StrinArrayList {
+
+	public static void main(String[] args) {
+		
+		ArrayList<String> s = new ArrayList<>();
+		
+		s.add("Bhopal");
+		s.add("Indore");
+		s.add("Betul");
+		s.add("Pune");
+		s.add("Mumbai");
+		s.add("Vidisha");
+		s.add(null);
+		
+		System.out.println(s);
+		
+		for(Object o : s) {
+			System.out.println("Arraylist includes city : "+ o);
+		}
+	}
+}
